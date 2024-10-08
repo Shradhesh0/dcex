3d4c880932991ac2efe3e2e3638a8274913c070a
Shradhesh0 <shradheshrg@gmail.com>
2024-10-09 02:42:24 +0530

    Auth Provider fixed

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,22 +1,9 @@
-// import NextAuth from 'next-auth'
-// import { authConfig } from '@/app/lib/auth'
+import NextAuth from 'next-auth'
+import { authConfig } from '@/app/lib/auth'
 
-// const handler =NextAuth(authConfig)
+const handler =NextAuth(authConfig)
 
-// export {handler as GET ,handler as POST}
-
-import NextAuth from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
-
-export default NextAuth({
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID ?? "",
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
-    }),
-  ],
-  // Other NextAuth configuration options, if needed
-});
+export {handler as GET ,handler as POST}
 
 console.log({
   clientId: process.env.GOOGLE_CLIENT_ID ?? "",
